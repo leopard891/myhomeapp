58e5c04c053e86a88c496b84df004e0cdf87bf52
leopard891 <leopard891@gmail.com>
2014-04-01 19:48:57 +0700

    moving serial port to io connection scope

--- webapp/server.js
+++ webapp/server.js
@@ -1,21 +1,22 @@
 // server.js
 
 // set up ======================================================================
-// get all the tools we need
 var express  = require('express');
 var app      = express();
 var server = require('http').createServer(app);
-var port     = process.env.PORT || 8080;
+var port     = process.env.PORT || 8000;
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash    = require('connect-flash');
-var io = require('socket.io').listen(server, { log: false });
+var io = require('socket.io').listen(server, { log: true });
 var serialport = require('serialport');
 var SerialPort = serialport.SerialPort;
 var nodemailer = require("nodemailer");
 var configDB = require('./config/database.js');
 var net = require('net');
 var http = require('http');
+var path = require('path');
+
 
 
 // configuration ===============================================================
@@ -26,6 +27,7 @@ var http = require('http');
 	app.configure(function() {
 
 		// set up our express application
+		app.use(express.static(path.join(__dirname,'public')));
 		app.use(express.logger('dev')); // log every request to the console
 		app.use(express.cookieParser()); // read cookies (needed for auth)
 		app.use(express.bodyParser()); // get information from html forms
@@ -43,190 +45,179 @@ var http = require('http');
 	// routes ======================================================================
 	require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
-function configuration()
-{
-	serialListener(debug);
-	initSocketIO(server, debug)
-}
+var portName = '/dev/tty.usbserial-A501JUTF'; //var portName = '/dev/tty.usbmodem1421';
+	
 
-function initSocketIO(server, debug)
-{
-	io.sockets.on('connection', function(socket, debug){
+	var sp = new SerialPort(portName, {
+		parser: serialport.parsers.readline("\r\n"),
+		baudRate : 9600,
+		dataBits : 8,
+		parity : 'none',
+		buffersize: 1024,
+		stopBits: 1,
+		flowControl : false,
+	});
+
+io.sockets.on('connection', function(socket, debug){
 	if(debug == false){
 	}
 
 	socket.on('button', function(data){
+		
 		var status = data.lampstatus;
 
-		console.log('data dari web: '+ status);
+		//console.log('data dari web: '+ status);
 
 		sp.open(function(){
 			if (status == '1')
 			{
 				sp.write('1');
-				console.log(status);
+				//console.log(status);
 			}
 			else if (status == '2')
 			{
 				sp.write('2');
-				console.log(status);
+				//console.log(status);
 				
 			}
 			else if (status == '3')
 			{
 				sp.write('3');
-				console.log(status);
+				//console.log(status);
 			}
 			else if (status == '4')
 			{
 				sp.write('4');
-				console.log(status);
+				//console.log(status);
 				
 			}
 			else if (status == '5')
 			{
 				sp.write('5');
-				console.log(status);
+				//console.log(status);
 			}
 			else if (status == '6')
 			{
 				sp.write('6');
-				console.log(status);
+				//console.log(status);
 				
 			}
 			else if (status == '7')
 			{
 				sp.write('7');
-				console.log(status);
+				//console.log(status);
 			}
 			else
 			{
 				sp.write('8');
-				console.log(status);
+				//console.log(status);
 			}
 		});
 	});
 
-	
-	socket.on('message', function(message){
-		io.sockets.broadcast.emit('message', message);
-		});
-	});
-}
 
-//listen serial port
+	sp.on('data', function(data){
+			var recdata = "";
 
-	
-
-//function serialListener(debug){
-	var portName = '/dev/tty.usbserial-A501JUTF'; //var portName = '/dev/tty.usbmodem1421';
-	var recdata = "";
-
-	var sp = new SerialPort(portName, {
-		parser: serialport.parsers.readline("\n"),
-		baudRate : 9600,
-		dataBits : 8,
-		parity : 'none',
-		stopBits: 1,
-		flowControl : false,
-	});
-
-	
-	//===== serial data container =====//
-	sp.on('open', function()
-	{
-		sp.on('data', function(data){
 			recdata = new Buffer(data, 'binary').toString('utf8');
-			//recdata = new serdata();
-			console.log('dalam  ' + recdata)
+			console.log('status  ' + recdata)
 
 			if (recdata.indexOf('i') == 2 ){
 				mailPlease();
-				console.log('email segera dikirim');
+				//console.log('email segera dikirim');
+				recdata = "";
 			}
 
 			else if(recdata.indexOf('a') == 1 && recdata.indexOf('0') == 2){
-				var listrik1 = '1';
-				//socket.emit('listrik', {listrikStatus:listrik1});
-				console.log('ke web - lampu 1 mati ' + listrik1);
+				socket.emit('listrik', {listrikStatus:1});
+				//console.log('ke web - lampu 1 mati ');
+				recdata = "";
 			}
 
 			else if(recdata.indexOf('a') == 1 && recdata.indexOf('1') == 2 ){
-				var listrik1 = '2';
-				//socket.emit('listrik', {listrikStatus:listrik2});
-				console.log('ke web - lampu 1 hidup');
-
+				socket.emit('listrik', {listrikStatus:2});
+				//console.log('ke web - lampu 1 hidup');
+				recdata = "";
 			}
 
 			else if(recdata.indexOf('b') == 1 && recdata.indexOf('0') == 2){
-				var listrik2 = '3';
-				//socket.emit('listrik', {listrikStatus:listrik3});
-				console.log('ke web - lampu 2 mati');
+				socket.emit('listrik', {listrikStatus:3});
+				//console.log('ke web - lampu 2 mati');
+				recdata = "";
 			}
 
 			else if(recdata.indexOf('b') == 1 && recdata.indexOf('1') == 2){
-				var listrik2 = '4';
-				//socket.emit('listrik', {listrikStatus:listrik4});
-				console.log('ke web - lampu 2 hidup');
+				socket.emit('listrik', {listrikStatus:4});
+				//console.log('ke web - lampu 2 hidup');
+				recdata = "";
 			}
 
 			else if(recdata.indexOf('c') == 1 && recdata.indexOf('0') == 2){
-				var listrik3 = '5';
-				//socket.emit('listrik', {listrikStatus:listrik5});
-				console.log('ke web - lampu 3 mati');
+				socket.emit('listrik', {listrikStatus:5});
+				//console.log('ke web - lampu 3 mati');
+				recdata = "";
 			}
 
 			else if(recdata.indexOf('c') == 1 && recdata.indexOf('1') == 2){
-				var listrik3 = '6';
-				//socket.emit('listrik', {listrikStatus:listrik6});
-				console.log('ke web - lampu 3 hidup');
+				socket.emit('listrik', {listrikStatus:6});
+				//console.log('ke web - lampu 3 hidup');
+				recdata = "";
 			}
 
 			else if(recdata.indexOf('d') == 1 && recdata.indexOf('0') == 2){
-				var listrik4 = '7';
-				//socket.emit('listrik', {listrikStatus:listrik7});
-				console.log('ke web - lampu 4 mati');
+				socket.emit('listrik', {listrikStatus:7});
+				//console.log('ke web - lampu 4 mati');
+				recdata = "";
 			}
 
 			else if(recdata.indexOf('d') == 1 && recdata.indexOf('1') == 2){
-				var listrik4 = '8';
-				//socket.emit('listrik', {listrikStatus:listrik8});
-				console.log('ke web - lampu 4 hidup');
+				socket.emit('listrik', {listrikStatus:8});
+				//console.log('ke web - lampu 4 hidup');
+				recdata = "";
 			}
 			else {
 				return false
 			}
 		});
+	
+	socket.on('disconnect', function(){
+		
+		console.log('user disconnect');
 	});
-
-//}
-/*function serdata{
-console.log('luar' + recdata);
-}*/
-
-//Ping webcam IP for knowing webcam status
-var hosts = [['173.194.69.94', 80], ['stackoverflow.com', 80], ['google.com', 444]];
-hosts.forEach(function(item) {
-    var sock = new net.Socket();
-    sock.setTimeout(2500);
-    sock.on('connect', function() {
-        console.log(item[0]+':'+item[1]+' is up.');
-        sock.destroy();
-    }).on('error', function(e) {
-        console.log(item[0]+':'+item[1]+' is down: ' + e.message);
-    }).on('timeout', function(e) {
-        console.log(item[0]+':'+item[1]+' is down: timeout');
-    }).connect(item[1], item[0]);
+	
 });
 
+	
+
+/*setInterval(function(){	
+//Ping webcam IP untuk mengetahui status server
+	var hosts = [['192.168.1.100', 8090], ['google.com', 80], ['google.com', 80]];
+	hosts.forEach(function(item) {
+	    var sock = new net.Socket();
+	    sock.setTimeout(2500);
+	    sock.on('connect', function() {
+	        console.log(item[0]+':'+item[1]+' is up.');
+	        sock.destroy();
+	    })
+	    .on('error', function(e) {
+	        console.log(item[0]+':'+item[1]+' is down: ' + e.message);
+	    })
+	    .on('timeout', function(e) {
+	        mailPlease();
+	        console.log(item[0]+':'+item[1]+' is down: timeout');
+	        
+	    }).connect(item[1], item[0]);
+	});
+},50000);
+*/
 
 
 // create reusable transport method (opens pool of SMTP connections)
 var smtpTransport = nodemailer.createTransport("SMTP",{
     service: "Gmail",
     auth: {
-        user: "----",
-        pass: "----"
+        user: "",
+        pass: ""
     }
 });
 
@@ -234,13 +225,13 @@ var smtpTransport = nodemailer.createTransport("SMTP",{
 var mailOptions = {
     from: " Security <leopard891@gmail.com>", // sender address
     to: "leopard891@gmail.com", // list of receivers
-    subject: "Intrusion Detected", // Subject line
-    text: "See your Camera", // plaintext body
+    subject: "Something wrong in your home", // Subject line
+    text: "See your Camera to see what happen", // plaintext body
     html: "<b>See your Camera</b>" // html body
 }
 
-function mailPlease(msg){
 
+function mailPlease(msg){
 // send mail with defined transport object
 smtpTransport.sendMail(mailOptions, function(error, response){
     if(error){
